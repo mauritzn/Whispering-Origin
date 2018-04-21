3c87caaf57d042cb19837b005c4482c01a91bf0e
Mauritz N <mauritz@europe.com>
2018-04-22 00:21:51 +0300

    Ändrade selectable inventory slots
    Gjorde så att man inte kan selecta tools mera (pga ändring av spelets mekanism)

--- src/classes/player.h
+++ src/classes/player.h
@@ -69,7 +69,6 @@ class Player {
     
     int _current_inventory_slot = 0;
     vector<inventory_slots> _inv_slots = {
-      TOOL_1, TOOL_2, TOOL_3, TOOL_4,
       SLOT_1, SLOT_2, SLOT_3, SLOT_4, SLOT_5, SLOT_6, SLOT_7, SLOT_8, SLOT_9, SLOT_10, SLOT_11, SLOT_12
     };
     
